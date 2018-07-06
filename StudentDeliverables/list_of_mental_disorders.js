b5ed933664e01f7ccec7cf4909be6716ee837563
Kate <tanhakate@gmail.com>
2018-07-05 21:55:34 -0400

    extracted birth, death and name mentions data

---
+++ StudentDeliverables/list_of_mental_disorders.js
@@ -0,0 +1,35 @@
+mentalDisorders = {"acute stress disorder", "adolescent antisocial behavior", \
+"agoraphobia", "alcohol dependence", "alcoholic hallucinosis", \
+"amnestic disorder", "anosognosia", "anterograde amnesia", "anxiety \
+disorder", "atelophobia", "attention deficit hyperactivity disorder", \
+"autophagia", "avoidant/restrictive food intake disorder", \
+"benzodiazepine dependence", "benzodiazepine withdrawal", \
+"bibliomania", "bipolar disorder", "bipolar ii disorder", "borderline \
+intellectual functioning", "brief psychotic disorder", \
+"caffeine-induced anxiety disorder", "cannabis dependence", \
+"catatonic schizophrenia", "claustrophobia", "cocaine intoxication", \
+"communication disorder", "cotard delusion", "delirium tremens", \
+"derealization", "desynchronosis", "diogenes syndrome", "dissociative \
+identity disorder", "dyslexia", "ekbom's syndrome", "encopresis", \
+"enuresis", "erotomania", "extreme delusion", "fregoli delusion", \
+"ganser syndrome", "generalized anxiety disorder", "grandiose \
+delusions", "hallucinogen persisting perception disorder", \
+"huntington's disease", "hypochondriasis", "insomnia", "kleptomania", \
+"lacunar amnesia", "major depressive episode", "malingering", \
+"mathematics disorder", "minor depressive disorder", "mixed episode", \
+"munchausen's syndrome", "narcolepsy", "neurodevelopmental disorder", \
+"night eating syndrome", "obsessive-compulsive disorder", \
+"obsessive-compulsive personality disorder", "ondine's curse", \
+"opioid dependence", "oppositional defiant disorder", "orthorexia", \
+"pain disorder", "paranoid personality disorder", "parkinson's \
+disease", "pathological gambling", "personality disorder", \
+"phencyclidine", "phobic disorder", "psychosis", "physical abuse", \
+"posttraumatic stress disorder", "premature ejaculation", "primary \
+insomnia", "psychogenic amnesia", "pyromania", "recurrent brief \
+depression", "residual schizophrenia", "rumination syndrome", \
+"schizoid personality disorder", "schizophreniform disorder", \
+"seasonal affective disorder", "selective mutism", "sexual \
+fetishism", "sexual sadism disorder", "sleep disorder", "sleep terror \
+disorder", "sleep paralysis", "social phobia", "somatoform disorder", \
+"stereotypic movement disorder", "stuttering", "tardive dyskinesia", \
+"transient global amnesia", "trichotillomania"}
